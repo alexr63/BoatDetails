aff4f147636c174df59db03d04642b740d5e0ec6
alexr63 <alex@resnik.co.ua>
2014-08-22 07:36:16 +0300

    initial commit

---
+++ BoatDetails.ascx.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) 2014 Cowrie
+
+using System;
+using DotNetNuke.Entities.Modules;
+using DotNetNuke.Services.Exceptions;
+using SelectedHotelsModel;
+
+namespace Cowrie.Modules.BoatDetails
+{
+    public partial class BoatDetails : PortalModuleBase
+    {
+        public Boat boat;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                try
+                {
+                    if (Request.QueryString["Id"] != null)
+                    {
+                        int id = int.Parse(Request.QueryString["Id"]);
+                        using (SelectedHotelsEntities db = new SelectedHotelsEntities())
+                        {
+                            boat = db.Products.Find(id) as Boat;
+                            if (boat != null)
+                            {
+                                ((DotNetNuke.Framework.CDefault)Page).Title = boat.Name;
+                                Repeater1.DataSource = boat.ProductImages;
+                                Repeater1.DataBind();
+                                RepeaterSpecs.DataSource = boat.ProductSpecs;
+                                RepeaterSpecs.DataBind();
+                                DataBind();
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Exceptions.ProcessModuleLoadException(this, ex);
+                }
+            }
+        }
+    }
+}
---
+++ Components/BoatDetailsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Web;
+using DotNetNuke;
+using DotNetNuke.Common.Utilities;
+using DotNetNuke.Entities.Modules;
+using DotNetNuke.Services.Search;
+using SelectedHotelsModel;
+
+namespace Cowrie.BoatDetails.Components
+{
+    public class BoatDetailsController : ISearchable
+	{
+		#region ISearchable Members
+
+		/// <summary>
+		/// Implements the search interface required to allow DNN to index/search the content of your
+		/// module
+		/// </summary>
+		/// <param name="modInfo"></param>
+		/// <returns></returns>
+		public DotNetNuke.Services.Search.SearchItemInfoCollection GetSearchItems(ModuleInfo modInfo)
+		{
+			SearchItemInfoCollection searchItems = new SearchItemInfoCollection();
+		    using (SelectedHotelsEntities db = new SelectedHotelsEntities())
+		    {
+				// TODO: implement!
+            }
+
+			return searchItems;
+		}
+
+		#endregion
+	}
+}
